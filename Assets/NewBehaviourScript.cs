21ad3ffb64038df9b8695320123a0690d61ccd39
Emil Essung <emil.essung@live.se>
2021-03-17 21:48:07 +0100

    add weapons

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -12,13 +12,21 @@ public class NewBehaviourScript : MonoBehaviour
     public int maxJumpCount = 6;
     public int maxSpeed = 5;
     public Vector2 jumpForce = new Vector2(0, 6);
+
     public Vector2 downForce = new Vector2(0, 6);
 
+    public bool grounded = false;
+
+    public float bigGunTimer = 3;
+    public float timer = 0;
+
+    private Vector3 linePos;
+    public LineRenderer lr;
 
     // Start is called before the first frame update
-    void Awake() {
+    void Awake()
+    {
         rb = GetComponent<Rigidbody2D>();
-    
     }
 
     // Update is called once per frame
@@ -27,39 +35,80 @@ public class NewBehaviourScript : MonoBehaviour
         float inputX = Input.GetAxisRaw("Horizontal");
 
         Vector2 movement = new Vector2(speed * inputX, 0);
-        movement += new Vector2(((rb.velocity.x < 0 && inputX > 0) ? 1 : 0)* turnmultiplier - ((rb.velocity.x > 0 && inputX < 0) ? 1 : 0 )*turnmultiplier, 0);
-        movement *= maxSpeed -0.01f*Mathf.Pow(rb.velocity.x, 2);
+        movement += new Vector2(((rb.velocity.x < 0 && inputX > 0) ? 1 : 0) * turnmultiplier - ((rb.velocity.x > 0 && inputX < 0) ? 1 : 0) * turnmultiplier, 0);
+        movement *= maxSpeed - 0.01f * Mathf.Pow(rb.velocity.x, 2);
 
 
-        rb.AddForce(movement * Time.deltaTime, ForceMode2D.Impulse); 
+        rb.AddForce(movement * Time.deltaTime, ForceMode2D.Impulse);
 
-        if (Input.GetAxisRaw("Vertical") > 0){
-            if (jumpCount < maxJumpCount){
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            if (jumpCount < maxJumpCount)
+            {
                 rb.AddForce(jumpForce, ForceMode2D.Impulse);
                 jumpCount++;
             }
 
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift)){
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (!grounded)
+            {
+                rb.AddForce(transform.up * -downForce, ForceMode2D.Impulse);
+            }
             transform.localScale = new Vector2(1f, 1.3f);
-            rb.AddForce(transform.up * -downForce, ForceMode2D.Impulse);
-        }else if(Input.GetKeyUp(KeyCode.LeftShift)){
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
             transform.localScale = new Vector2(1f, 2f);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             shoot();
+        }
+
+        if (true){
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, linePos);
+        }
+
+        if (timer > 0){
+            timer -= Time.deltaTime;
+        }else if (timer <= 0){
+            lr.positionCount = 0;
+        }
+
+    }
+
+    void shoot()
+    {
+        Vector2 dir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+        GameObject projectile = Instantiate(prefab, transform.position, Quaternion.identity);
+
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir);
+        if (timer <= 0){
+            lr.positionCount = 2;
+            drawLine(Input.mousePosition);
         }
     }
 
-    void shoot() {
-            GameObject projectile = Instantiate(prefab, transform.position, Quaternion.identity);
-            projectile.GetComponent<projectile>().Setup(transform.position);
+   void drawLine(Vector3 mousePos){
+        linePos = Camera.main.ScreenToWorldPoint(mousePos);
+        timer = bigGunTimer;
+   }
+
+    void OnCollisionEnter2D()
+    {
+        jumpCount = 0;
+        grounded = true;
     }
 
-    void OnCollisionEnter2D(){
-        jumpCount = 0;
+    void OnCollisionExit2D()
+    {
+        grounded = false;
     }
 
 }
--- Assets/projectile.cs
+++ Assets/projectile.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 public class projectile : MonoBehaviour
 {
     public Rigidbody2D rb;
+
+    public float projectileSpeed = 2f;
     Vector3 dir;
 
     // Start is called before the first frame update
-    public void Setup(Vector3 parentPos)
+    public void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
-        dir = Input.mousePosition - parentPos;
-        dir = dir.normalized;
+        dir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 
         float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         if (n < 0) n += 360;
 
-
-        transform.eulerAngles = new Vector3(0, 0, n);
+        Quaternion q = Quaternion.AngleAxis(n, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime);
 
     }
     // Update is called once per frame
     void Update()
     {
-        transform.position += dir * 50f * Time.deltaTime;
-        Debug.Log(dir);
+        transform.position += dir * projectileSpeed * Time.deltaTime;
     }
 }
